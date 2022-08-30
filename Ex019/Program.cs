b9aa182c76d3033df9ad563f871435ac8654f393
Katya <katvas785@gmail.com>
2022-08-30 21:00:33 +0300

    Ex019

---
+++ Ex019/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = 14212;
+int duplicate = i;
+int reverse = 0;
+while (i > 0)
+{
+    int a = i % 10;
+    reverse = reverse * 10 + a;
+    i = i / 10;
+}
+if(duplicate == reverse)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
